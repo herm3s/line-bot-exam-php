e68534bea866da83d3776869f3a9e0ab224ca7f0
herm3s <angeluster@gmail.com>
2018-04-17 14:55:35 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -3,16 +3,18 @@
 
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-use LINEBotTiny;
-
 
 $access_token = 'FZldEem8ostD63IqQ5NQ0mZnYHK/NSzQutlkVIFLa9rRzFYQ3SXMvnzr6gM/rrBPK4wdLlSgA8Ba7vOJMajRtzAYouW9l8rQ3xlQeiDlBS48fUbw41nCul84q4NKVpQ53r/5mF4CUx1CNQfS3+iBbwdB04t89/1O/w1cDnyilFU=';
 
-$bot = new LINEBotTiny();
 
-$stat = $bot->profil('U669d211bf92aca29db6e6fc4dafab857');
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'e5d464aa428fdb58ede0e1a877108551']);
 
-echo $stat;
-  
+$res = $bot->pushMessage(
+            'DESTINATION',
+            new VideoMessageBuilder('https://www.quirksmode.org/html5/videos/big_buck_bunny.mp4', 'https://thumb.ibb.co/isTQZ7/prev.jpg')
+        );
+
+  echo $res;
   
 ?>
