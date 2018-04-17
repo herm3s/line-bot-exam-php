590a1afac49fa509685001196a8d0c4698e7a167
herm3s <angeluster@gmail.com>
2018-04-17 14:45:24 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -8,11 +8,11 @@ use LINEBotTiny;
 
 $access_token = 'FZldEem8ostD63IqQ5NQ0mZnYHK/NSzQutlkVIFLa9rRzFYQ3SXMvnzr6gM/rrBPK4wdLlSgA8Ba7vOJMajRtzAYouW9l8rQ3xlQeiDlBS48fUbw41nCul84q4NKVpQ53r/5mF4CUx1CNQfS3+iBbwdB04t89/1O/w1cDnyilFU=';
 
+$bot = new LINEBotTiny();
 
+$stat = $bot->profil('U669d211bf92aca29db6e6fc4dafab857')
 
-$stat = LINEBotTiny->profil('U669d211bf92aca29db6e6fc4dafab857')
-
-  echo $stat;
+echo $stat;
   
   
 ?>
