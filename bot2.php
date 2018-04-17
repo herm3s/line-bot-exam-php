5fa628d1d88756f3165789226e3c54240f3d99f3
herm3s <angeluster@gmail.com>
2018-04-17 14:38:04 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -6,9 +6,9 @@ include_once '\vendor\linecorp\line-bot-sdk\tests\LINEBot\SendVideoTest.php';
 use LINE\Tests\LINEBot;
 
 $send1 = new SendVideoTest();
-$send1->testPushVideo();
+&stat = $send1->testPushVideo();
 
-echo $send1;
+echo &stat;
 
   
   
