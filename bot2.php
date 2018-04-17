720d397d29d763256c81d9f6197a9ac4f2a5940d
herm3s <angeluster@gmail.com>
2018-04-17 14:44:17 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -1,13 +1,18 @@
 
 <?php
 
-include_once 'line-bot-exam-php/vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php';
-
+require "vendor/autoload.php";
+require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 use LINEBotTiny;
 
-profil($userId)
 
-  
+$access_token = 'FZldEem8ostD63IqQ5NQ0mZnYHK/NSzQutlkVIFLa9rRzFYQ3SXMvnzr6gM/rrBPK4wdLlSgA8Ba7vOJMajRtzAYouW9l8rQ3xlQeiDlBS48fUbw41nCul84q4NKVpQ53r/5mF4CUx1CNQfS3+iBbwdB04t89/1O/w1cDnyilFU=';
+
+
+
+$stat = LINEBotTiny->profil('U669d211bf92aca29db6e6fc4dafab857')
+
+  echo $stat;
   
   
 ?>
