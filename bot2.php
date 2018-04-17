8efd44219b8d717d634d1437056b0cdb721738db
herm3s <angeluster@gmail.com>
2018-04-17 14:09:25 +0700

    Create bot2.php

---
+++ bot2.php
@@ -0,0 +1,13 @@
+
+<?php
+
+include_once '\vendor\linecorp\line-bot-sdk\tests\LINEBot\SendVideoTest.php';
+
+use SendVideoTest;
+
+$send1 = new SendVideoTest();
+$send1->testReplyVideo();
+
+echo $send1;
+
+?>
