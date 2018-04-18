0a2c39251ce819b659d0b839fe94ca1a4280d307
herm3s <angeluster@gmail.com>
2018-04-18 09:19:13 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -10,6 +10,8 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
+
+
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
