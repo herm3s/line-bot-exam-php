ac214211c4368a093590c2dbfd6cf8d99e74b0f1
herm3s <angeluster@gmail.com>
2018-04-18 10:53:17 +0700

    Update bot3.php

--- bot3.php
+++ bot3.php
@@ -175,9 +175,9 @@ if(!is_null($events)){
                     file_put_contents($fileFullSavePath,$dataBinary); // ทำการบันทึกไฟล์
                     //$textReplyMessage = "บันทึกไฟล์เรียบร้อยแล้ว $fileNameSave";
                     $textReplyMessage = "บันทึกไฟล์เรียบร้อยแล้ว $fileFullSavePath";
-                    //$replyData = new TextMessageBuilder($textReplyMessage);
+                    $replyData = new TextMessageBuilder($textReplyMessage);
                  
-                    $replyData = new TextMessageBuilder($content);
+                    //$replyData = new TextMessageBuilder($content);
                     
                  
                     //add
